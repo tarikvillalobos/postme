25a90d8c3d4b20d5863a8e13c310eb4cee9a6b2f
Tarik Villalobos <tarik.villalobos@gmail.com>
2025-12-14 20:08:48 -0300

    Refactor Home component layout to use a grid structure, enhancing responsiveness and visual organization. Updated background colors for improved aesthetics.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Postme
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <main className="grid min-h-screen w-full max-w-6xl grid-cols-[1.25fr_3.5fr_1.25fr]">
+        
+        <section className="bg-green-100" />
+
+        <section className="bg-white dark:bg-zinc-900 p-8">
+          Postme
+        </section>
+
+        <section className="bg-green-100" />
+
       </main>
     </div>
-  );
+  )
 }
